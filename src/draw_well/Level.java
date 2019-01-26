c669816e9261e985c0b5202055e4254145aaddc0
Milan Zafirovic <zafirovicmilan2@gmail.com>
2019-01-27 00:08:13 +0100

    added class for the level of the draw well

---
+++ src/draw_well/Level.java
@@ -0,0 +1,77 @@
+package draw_well;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Material;
+import javafx.scene.shape.Box;
+
+public class Level extends Group {
+
+    private Box [][] boxes;
+    private int boxNumX, boxNumY;
+    private int visibleBoxesCnt = 0;
+
+    public Level(double boxDimension, int boxNumX, int boxNumY) {
+        this.boxNumX = boxNumX;
+        this.boxNumY = boxNumY;
+        boxes = new Box[boxNumY][];
+        double tx = 0, ty = 0;
+        for (int i = 0; i < boxNumY; i++) {
+            boxes[i] = new Box[boxNumX];
+            for (int j = 0; j < boxNumX; j++) {
+                Box box = new Box(boxDimension, boxDimension, boxDimension);
+                box.setTranslateX(box.getTranslateX() + tx);
+                box.setTranslateY(box.getTranslateY() + ty);
+                boxes[i][j] = box;
+                box.setVisible(false);
+                getChildren().add(box);
+                tx += boxDimension;
+            }
+            tx = 0;
+            ty += boxDimension;
+        }
+    }
+
+    /**
+     * Set material for the whole level
+     * @param material
+     */
+    public void setMaterial(Material material){
+        for (int i = 0; i < boxNumY; i++) {
+            for (int j = 0; j < boxNumX; j++) {
+                boxes[i][j].setMaterial(material);
+            }
+        }
+    }
+
+    /**
+     * Set visibility for just one box
+     */
+    public void setVisible(int i, int j){
+        if(i>=0 && j>=0 && i<boxNumX &&  j<boxNumY)
+            if(!boxes[i][j].isVisible()){
+                boxes[i][j].setVisible(true);
+                ++visibleBoxesCnt;
+            }
+    }
+
+    /**
+     * Make the whole level invisible
+     */
+    public void setInvisible(){
+        for (int i = 0; i < boxNumY; i++) {
+            for (int j = 0; j < boxNumX; j++) {
+                boxes[i][j].setVisible(false);
+            }
+        }
+        visibleBoxesCnt = 0;
+    }
+
+    /**
+     * Checks if all boxes at this level are visible
+     */
+    public boolean isWholeLevelVisible(){
+        return visibleBoxesCnt == (boxNumX * boxNumY);
+    }
+
+
+}
