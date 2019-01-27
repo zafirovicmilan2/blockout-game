7e9a4e9e6320fffd534364e94f5c6b1648a03404
Milan Zafirovic <zafirovicmilan2@gmail.com>
2019-01-27 21:40:45 +0100

    added possibility to remove level from draw-well

--- src/draw_well/DrawWell.java
+++ src/draw_well/DrawWell.java
@@ -2,10 +2,14 @@ package draw_well;
 
 import geometry.Coordinates;
 import geometry.Geometry;
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
 import javafx.scene.Group;
 import javafx.scene.paint.Material;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DrawWell extends Group {
@@ -15,11 +19,15 @@ public class DrawWell extends Group {
     private double boxDimension;
 
     public DrawWell(double boxDimension, int boxNumX, int boxNumY, int boxNumZ, int extBoxNumZ, Material[] matLevel) {
+
         this.boxDimension = boxDimension;
         this.matLevel = matLevel; // TODO 1)update material setting, 2)check if boxNumZ==len(matLevel)
-        levels = new ArrayList<>();
+
         frame = new Frame(boxDimension, boxNumX, boxNumY, boxNumZ + extBoxNumZ);
         Geometry.positionTo(frame, Frame.WIRE_RADIUS, Frame.WIRE_RADIUS, Frame.WIRE_RADIUS);
+        getChildren().add(frame);
+
+        levels = new ArrayList<>();
         double tz = 0;
         for (int i = 0; i < boxNumZ; i++) {
             Level level = new Level(boxDimension, boxNumX, boxNumY);
@@ -43,8 +51,51 @@ public class DrawWell extends Group {
         return new Coordinates<>(i, j, k);
     }
 
+    /**
+     * make the box(with this indexes) visible
+     */
     public void setVisible(Coordinates<Integer> coordinates){
         // TODO check index out of bounds
         levels.get(coordinates.getI()).setVisible(coordinates.getJ(),coordinates.getK());
+    }
+
+    private void removeLevel(int levelIndex){
+        // TODO check index out of bounds
+        //bring down all levels above current level
+        ParallelTransition pt = new ParallelTransition();
+        for (int j = levelIndex + 1; j < levels.size(); j++) {
+            TranslateTransition tt = new TranslateTransition(Duration.seconds(1.0), levels.get(j));
+            tt.setByZ(-boxDimension);
+            pt.getChildren().add(tt);
+        }
+
+        Level removed = levels.remove(levelIndex);
+        removed.setInvisible();
+        //move removed level to the top; TODO check if this must be done after ParallelTransition
+        removed.setTranslateZ(removed.getTranslateZ() + boxDimension * (getHeightBoxesNum() - levelIndex));
+        levels.add(removed);
+
+        pt.play();
+
+    }
+
+    public void removeLevels(int... levelIndexes){
+        // TODO check if levelIndexes has only unique elements(i.e. not OK:[1,2,2] OK:[1,2,3])
+        Arrays.sort(levelIndexes);
+        for (int i = levelIndexes.length - 1; i >= 0; i--) {
+            removeLevel(i);
+        }
+
+        assignMaterial();
+    }
+
+    private int getHeightBoxesNum(){
+        return levels.size();
+    }
+
+    private void assignMaterial(){
+        for (int i = 0; i < levels.size(); i++) {
+            levels.get(i).setMaterial(matLevel[i]);
+        }
     }
 }
